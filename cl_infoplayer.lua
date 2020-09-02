a0dd88d1892e573d164284097f82189506e80338
IFNET <39136768+IFN3T@users.noreply.github.com>
2020-09-02 14:02:54 +0200

    Update cl_infoplayer.lua

--- cl_infoplayer.lua
+++ cl_infoplayer.lua
@@ -7,9 +7,9 @@ for i, v in ipairs( player.GetAll() ) do
 	print("SteamID64: " .. v:SteamID64() or "???" .. "\n")
 	print("Team: " .. v:Team() or "???" .. "\n")
 	local max = 100 * v:Health() / v:GetMaxHealth() or ""
-	print("Vie: " .. v:Health() or "???" .. "/" .. v:GetMaxHealth() or "" .. " - " .. max .. "%\n")
+	print("Vie: " .. v:Health() or "???" .. "/" .. v:GetMaxHealth() or "???" .. " - " .. max .. "%\n")
 	print("Grade: " .. v:GetUserGroup() or "???" .. "\n")
-	print("[DARKRP] Money: " .. v:getDarkRPVar("money") or 0 .. "\n")
+	print("[DARKRP] Money: " .. v:getDarkRPVar("money") or "???" .. "\n")
 	
 end
 
